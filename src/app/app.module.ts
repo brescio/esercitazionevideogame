88991292bb991dd6716eb23d3a6a9f0ca19903b2
brescio <57990413+brescio@users.noreply.github.com>
2020-03-23 11:22:09 +0100

    altre correzioni

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameListComponent } from './components/game-list/game-list.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { EditGameComponent } from './components/edit-game/edit-game.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
-import { GameListComponent } from './components/game-list/game-list.component';
-import { MenuItem } from './models/menu-item.interface/menu-item.interface.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GameListComponent,
     MenuComponent,
     EditGameComponent,
-    GameDetailComponent,
-    GameListComponent,
-    MenuItem.InterfaceComponent
+    GameDetailComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter} from '@angular/core';
 import { MenuItem } from 'src/app/models/menu-item.interface';
-
-
-
+import { isNgTemplate } from '@angular/compiler';
   @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.scss']
   })
   export class MenuComponent implements OnInit {
+@Output()
+selectedMenuItem:EventEmitter<number>=new EventEmitter();
   menuList: MenuItem[] = [
     { id: 1, description: 'Home', selected: true},
     { id: 2, description: 'Lista giochi', selected: false},
@@ -20,3 +20,19 @@ import { MenuItem } from 'src/app/models/menu-item.interface';
   ngOnInit(): void {
 
   }
+  selectedComponents(id:number){
+    for(let menuItem of this.menuList){
+      menuItem.selected=id ===menuItem.id;
+    }
+    const arr = this.menuList.filter(item =>{
+      return item.selected;
+    })
+    const asd = this.menuList.find(item =>{
+      return item.selected;
+    })
+    this.menuList.some(item =>{
+      return item.selected;
+    })
+    this.selectedMenuItem.emit(id);
+  }
+}
