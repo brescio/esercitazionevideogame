ab2a4e767278e0ff9286655f5547af3d4cdaca63
brescio <57990413+brescio@users.noreply.github.com>
2020-03-23 10:31:20 +0100

    creazione game-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { EditGameComponent } from './components/edit-game/edit-game.component';
 import { GameDetailComponent } from './components/game-detail/game-detail.component';
+import { GameListComponent } from './components/game-list/game-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     EditGameComponent,
-    GameDetailComponent
+    GameDetailComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule
